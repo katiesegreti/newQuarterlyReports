ac703f04a1603f22b748d2155377d14ffbff1612
Katie <katie.segreti@gmail.com>
2018-04-04 01:39:19 -0400

    Update more functions

--- mandateFunctions.R
+++ mandateFunctions.R
@@ -9,284 +9,146 @@ pasteNumber <- function(x, y) {  paste(x, y)}
 totalN <- function(df, rowVar, colVar) {
   x <- enquo(rowVar)
   y <- enquo(colVar)
-  df %>%
+  df1 <- df %>%
     group_by(!!x, !!y) %>%
     summarise(total = n()) %>%
-    spread(!!y, total)
+    spread(!!y, total) %>%
+    replace(., is.na(.), 0)
+  return(cbind(df1, Total = rowSums(df1[-1], na.rm = TRUE)))
 }
+
 ##total value of mandates: df, 2 variables
 totalSum <- function(df, rowVar, colVar) {
   x <- enquo(rowVar)
   y <- enquo(colVar)
-  df %>%
+  df1 <- df %>%
     group_by(!!x, !!y) %>%
     summarise(total = sum(MandateSizeAmount)) %>%
-    spread(!!y, total)
+    spread(!!y, total) %>%
+    replace(., is.na(.), 0)
+  return(cbind(df1, Total = rowSums(df1[-1], na.rm = TRUE)))
 }
-#### WRITE A GLOBAL FUNCTION TO ADD 1 TO tbl2 COLUMN NAMES SO YOU CAN JOIN 
 
-
-#-----function
-#-----------QUARTERLY MANDATE SUMMARY - POTENTIAL, NEW, COMPLETED
 #----FUNCTION: POTENTIAL NEW COMPLETED ASSET CLASS QUARTER MATRIX  USQ,USPNC1,USPNC2
 ##updated
 PNCtable <- function(df) {
-  tbl1 <- df %>% 
-    group_by(MainAssetClass, SearchStatus) %>%
-    summarise(total = n()) %>%
-    spread(SearchStatus, total)
-  tbl2 <- df %>% 
-    group_by(MainAssetClass, SearchStatus) %>%
-    summarise(total = sum(MandateSizeAmount)) %>%
-    spread(SearchStatus, total)
+  tbl1 <- totalN(df, MainAssetClass, SearchStatus)
+  tbl2 <- totalSum(df, MainAssetClass, SearchStatus)
   colznmz <- colnames(tbl2)[-1]
   colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
   combined <- tbl1 %>% left_join(tbl2)
-  combined <- combined[,c(1,4,7,3,6,2,5)]
+  combined <- combined[,c(1,4,8,3,7,2,6,5,9)]
   return(combined)
 }
 USPNC <- PNCtable(USQ)
 
-
-
-
-
-
-
-
-
 ####### FTPtable the new way 
 ## still have to get totals column
 FTtableNEW <- function(df) {
-  tbl1 <- df %>%
-    group_by(FundType, MainAssetClass) %>%
-    summarise(total = n()) %>%
-    spread(MainAssetClass, total)
-  tbl2 <- df %>%
-    group_by(FundType, MandateRegion) %>%
-    summarise(total = n()) %>%
-    spread(MandateRegion, total)
+  tbl1 <- totalN(df, FundType, MainAssetClass)
+  tbl2 <- totalN(df, FundType, MandateRegion)
   combined <- tbl1 %>% left_join(tbl2)
   return(combined)
 }
 USP_FT <- FTtableNEW(USQP)
 
-
-
 ### updated
 #---- Asset class by investment region
 ACbyMRtable <- function(df) {
-  tbl1 <- df %>%
-    group_by(MainAssetClass, MandateRegion) %>%
-    summarise(total = n()) %>%
-    spread(MandateRegion, total)
-  tbl2 <- df %>%
-    group_by(MainAssetClass, MandateRegion) %>%
-    summarise(total = sum(MandateSizeAmount)) %>%
-    spread(MandateRegion, total)
-  colznmz <- colnames(tbl2)[-1]
-  colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
-  tbl1 %>% left_join(tbl2)
-}
-
-ACbyMRtable1 <- function(df) {
   tbl1 <- totalN(df, MainAssetClass, MandateRegion)
   tbl2 <- totalSum(df, MainAssetClass, MandateRegion)
   colznmz <- colnames(tbl2)[-1]
   colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
   tbl1 %>% left_join(tbl2)
 }
-USQAC1 <- ACbyMRtable1(USQP)
+
+USQAC1 <- ACbyMRtable(USQP)
 
 
-
-### STILL NEED TO UPDATE
+### UPDATED
 #---- SubAsset class by investment region for assete class
-SACbyMRtable <- function(df1, assetclass){
-  dftemp <- df1[df1$MainAssetClass == assetclass,]
-  SAC <- (levels(droplevels(dftemp$SubAssetClass)))
-  
-  MR <- (levels(droplevels(dftemp$MandateRegion)))
-  MR2 <- rep(MR,times=1, each=2)
-  MR2 <- c(MR2, "total#", "total$")
-  z <- rep(0, length(SAC)*length(MR2))
-  #create matrix
-  m1 <- matrix(z, nrow=length(SAC), ncol=length(MR2))
-  rownames(m1) <- SAC
-  colnames(m1) <- MR2
-  #fill matrix
-  for(i in 1:length(SAC)){
-    for(j in 1:length(MR2)){
-      if(is.odd(j)){
-        m1[i,j] <- nrow(dftemp[dftemp$SubAssetClass==SAC[i]&dftemp$MandateRegion==MR2[j],])
-      }else{
-        m1[i,j] <- sum(dftemp[dftemp$SubAssetClass==SAC[i]&dftemp$MandateRegion==MR2[j],"MandateSizeAmount"])
-      }
-    }
-  }
-  #get totals
-  for(i in 1:length(SAC)){
-    m1[i, length(MR2)-1] <- nrow(dftemp[dftemp$SubAssetClass==SAC[i],])
-    m1[i, length(MR2)] <- sum(dftemp[dftemp$SubAssetClass==SAC[i],"MandateSizeAmount"])
-  }
-  return(m1)
+SACbyMRtable <- function(df1, assetclass) {
+  dftemp <- df1 %>% filter(MainAssetClass == assetclass)
+  tbl1 <- totalN(dftemp, SubAssetClass, MandateRegion)
+  tbl2 <- totalSum(dftemp, SubAssetClass, MandateRegion)
+  colznmz <- colnames(tbl2)[-1]
+  colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
+  tbl1 %>% left_join(tbl2)
 }
 
-### STILL NEED TO UPDATE
+SACtest1 <- SACbyMRtable(mandates, "Commodities")
+
+
+### UPDATED
 #---- Investment approach by investment region for equity and fixed Income
-IAbyMRtable <- function(df1, assetclass){
-  dftemp <- df1[df1$MainAssetClass == assetclass,]
-  IA <- (levels(droplevels(dftemp$InvestmentApproach)))
-  
-  MR <- (levels(droplevels(dftemp$MandateRegion)))
-  MR2 <- rep(MR,times=1, each=2)
-  MR2 <- c(MR2, "Total#", "Total$")
-  z <- rep(0, length(IA)*length(MR2))
-  #create matrix
-  m1 <- matrix(z, nrow=length(IA), ncol=length(MR2))
-  rownames(m1) <- IA
-  colnames(m1) <- MR2
-  #fill matrix
-  for(i in 1:length(IA)){
-    for(j in 1:length(MR2)){
-      if(is.odd(j)){
-        m1[i,j] <- nrow(dftemp[dftemp$InvestmentApproach==IA[i]&dftemp$MandateRegion==MR2[j],])
-      }else{
-        m1[i,j] <- sum(dftemp[dftemp$InvestmentApproach==IA[i]&dftemp$MandateRegion==MR2[j],"MandateSizeAmount"])
-      }
-    }
-  }
-  #get totals
-  for(i in 1:length(IA)){
-    m1[i, length(MR2)-1] <- nrow(dftemp[dftemp$InvestmentApproach==IA[i],])
-    m1[i, length(MR2)] <- sum(dftemp[dftemp$InvestmentApproach==IA[i],"MandateSizeAmount"])
-  }
-  return(m1)
+
+IAbyMRtable <- function(df1, assetclass) {
+  dftemp <- df1 %>% filter(MainAssetClass == assetclass)
+  tbl1 <- totalN(dftemp, InvestmentApproach, MandateRegion)
+  tbl2 <- totalSum(dftemp, InvestmentApproach, MandateRegion)
+  colznmz <- colnames(tbl2)[-1]
+  colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
+  tbl1 %>% left_join(tbl2)
 }
-
+IAtest <- IAbyMRtable(US12mo, "Equity")
 
   
-### STILL NEED TO UPDATE 
+### UPDATED
 #---- style by investment region for equity 
-EQStyletable <- function(df1){
-  dftemp <- df1[df1$MainAssetClass == "Equity",]
-  style <- (levels(droplevels(dftemp$Style)))
-  
-  MR <- (levels(droplevels(dftemp$MandateRegion)))
-  MR2 <- rep(MR,times=1, each=2)
-  MR2 <- c(MR2, "Totla#", "Total$")
-  z <- rep(0, length(style)*length(MR2))
-  #create matrix
-  m1 <- matrix(z, nrow=length(style), ncol=length(MR2))
-  rownames(m1) <- style
-  colnames(m1) <- MR2
-  #fill matrix
-  for(i in 1:length(style)){
-    for(j in 1:length(MR2)){
-      if(is.odd(j)){
-        m1[i,j] <- nrow(dftemp[dftemp$Style==style[i]&dftemp$MandateRegion==MR2[j],])
-      }else{
-        m1[i,j] <- sum(dftemp[dftemp$Style==style[i]&dftemp$MandateRegion==MR2[j],"MandateSizeAmount"])
-      }
-    }
-  }
-  #get totals
-  for(i in 1:length(style)){
-    m1[i, length(MR2)-1] <- nrow(dftemp[dftemp$Style==style[i],])
-    m1[i, length(MR2)] <- sum(dftemp[dftemp$Style==style[i],"MandateSizeAmount"])
-  }
-  return(m1)
+
+EQStyletable <- function(df1) {
+  dftemp <- df1 %>% filter(MainAssetClass == "Equity")
+  tbl1 <- totalN(dftemp, Style, MandateRegion)
+  tbl2 <- totalSum(dftemp, Style, MandateRegion)
+  colznmz <- colnames(tbl2)[-1]
+  colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
+  tbl1 %>% left_join(tbl2)
 }
-### STILL NEED TO UPDATE
-EQCaptable <- function(df1){
-  dftemp <- df1[df1$MainAssetClass == "Equity",]
-  stylee <- levels(droplevels(dftemp$Style))
-  MR <- levels(droplevels(dftemp$MandateRegion))
-  MR2 <- rep(MR, times=1, each=2)
-  MR2 <- c(MR2, "Total#", "Total$")
-  stylez <- vector()   #for full list of main asset classes
-  CAP <- vector() 
-  CAPz <- vector() #for full list of sub asset classes by main asset class
-  capstylez <- vector() #combined main and sub for rownames
-  sNum <- vector() #number of sub asset classes in each stylee
-  #get sub asset class list (CAPz)
-  for(i in 1:length(stylee)){
-    dftempa <- dftemp[dftemp$Style==stylee[i],]
-    CAP <- levels(droplevels(dftempa$CapSize))
-    sNum <- c(sNum, length(CAP))
-    CAPz <- c(CAPz, CAP)
-  }
-  #get main asset class list (stylez)
-  for(i in 1:length(sNum)){
-    stylez <- c(stylez, rep(stylee[i], times=1,each=sNum[i]))
-  }
-  #fill matrix
-  z <- rep(0, length(CAPz)*length(MR2))
-  m1 <- matrix(z, nrow=length(CAPz), ncol=length(MR2))
-  colnames(m1) <- MR2
-  for(i in 1:length(CAPz)){
-    for(j in 1:length(MR2)){
-      if(is.odd(j)){
-        m1[i,j] <- nrow(dftemp[dftemp$Style==stylez[i]&dftemp$CapSize==CAPz[i]&dftemp$MandateRegion==MR2[j],])
-      }else{
-        m1[i,j] <- sum(dftemp[dftemp$Style==stylez[i]&dftemp$CapSize==CAPz[i]&dftemp$MandateRegion==MR2[j],"MandateSizeAmount"])
-      }
-    }
-    capstylez <- c(capstylez, paste(stylez[i],CAPz[i]))
-  }
-  rownames(m1) <- capstylez
-  #get totals
-  for(i in 1:length(capstylez)){
-    m1[i,length(MR2)-1] <- nrow(dftemp[dftemp$Style==stylez[i]&dftemp$CapSize==CAPz[i],])
-    m1[i, length(MR2)] <- sum(dftemp[dftemp$Style==stylez[i]&dftemp$CapSize==CAPz[i], "MandateSizeAmount"])
-  }
-  return(m1)
+eqstest <- EQStyletable(mandates)
+
+### UPDATED
+EQCaptable <- function(df1) {
+  dftemp <- df1 %>% filter(MainAssetClass == "Equity")
+  tbl1 <- totalN(dftemp, CapSize, MandateRegion)
+  tbl2 <- totalSum(dftemp, CapSize, MandateRegion)
+  colznmz <- colnames(tbl2)[-1]
+  colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
+  tbl1 %>% left_join(tbl2)
 }
-### STILL NEED TO UPDATE
-AltsTable <- function(df1){
-  dftemp <- df1[df1$MainAssetClass%in%alternatives,]
-  AC <- levels(droplevels(dftemp$MainAssetClass))
-  MR <- levels(droplevels(dftemp$MandateRegion))
-  MR2 <- rep(MR, times=1, each=2)
-  MR2 <- c(MR2, "Total#", "Total$")
-  ACz <- vector()   #for full list of main asset classes
-  SAC <- vector() 
-  SACz <- vector() #for full list of sub asset classes by main asset class
-  ACSACz <- vector() #combined main and sub for rownames
-  sNum <- vector() #number of sub asset classes in each AC
-  #get sub asset class list (SACz)
-  for(i in 1:length(AC)){
-    dftempa <- dftemp[dftemp$MainAssetClass==AC[i],]
-    SAC <- levels(droplevels(dftempa$SubAssetClass))
-    sNum <- c(sNum, length(SAC))
-    SACz <- c(SACz, SAC)
-  }
-  #get main asset class list (ACz)
-  for(i in 1:length(sNum)){
-    ACz <- c(ACz, rep(AC[i], times=1,each=sNum[i]))
-  }
-  #fill matrix
-  z <- rep(0, length(SACz)*length(MR2))
-  m1 <- matrix(z, nrow=length(SACz), ncol=length(MR2))
-  colnames(m1) <- MR2
-  for(i in 1:length(SACz)){
-    for(j in 1:length(MR2)){
-      if(is.odd(j)){
-        m1[i,j] <- nrow(dftemp[dftemp$MainAssetClass==ACz[i]&dftemp$SubAssetClass==SACz[i]&dftemp$MandateRegion==MR2[j],])
-      }else{
-        m1[i,j] <- sum(dftemp[dftemp$MainAssetClass==ACz[i]&dftemp$SubAssetClass==SACz[i]&dftemp$MandateRegion==MR2[j],"MandateSizeAmount"])
-      }
-    }
-    ACSACz <- c(ACSACz, paste(ACz[i],SACz[i]))
-  }
-  rownames(m1) <- ACSACz
-  #get totals
-  for(i in 1:length(ACSACz)){
-    m1[i,length(MR2)-1] <- nrow(dftemp[dftemp$MainAssetClass==ACz[i]&dftemp$SubAssetClass==SACz[i],])
-    m1[i, length(MR2)] <- sum(dftemp[dftemp$MainAssetClass==ACz[i]&dftemp$SubAssetClass==SACz[i], "MandateSizeAmount"])
-  }
-  return(m1)
+eqctest <- EQCaptable(mandates)
+
+
+### UPDATED
+# alts table
+
+AltsTable <- function(df1) {
+  tbl1 <- df1 %>%
+    filter(MainAssetClass %in% alternatives) %>%
+    group_by(MainAssetClass, SubAssetClass, MandateRegion) %>%
+    summarise(total = n()) %>%
+    replace_na(list(SubAssetClass = "(Unspecified)")) %>%
+    mutate(MainSub = paste(MainAssetClass, SubAssetClass)) %>%
+    spread(MandateRegion, total) %>%
+    replace(., is.na(.), 0) 
+  tbl1 <- tbl1[, -c(1, 2)]
+  tbl1 <- cbind(tbl1, Total = rowSums(tbl1[-1], na.rm = TRUE))
+  tbl2 <- df1 %>%
+    filter(MainAssetClass %in% alternatives) %>%
+    group_by(MainAssetClass, SubAssetClass, MandateRegion) %>%
+    summarise(total = sum(MandateSizeAmount)) %>%
+    replace_na(list(SubAssetClass = "(Unspecified)")) %>%
+    mutate(MainSub = paste(MainAssetClass, SubAssetClass)) %>%
+    spread(MandateRegion, total) %>%
+    replace(., is.na(.), 0) 
+  tbl2 <- tbl2[, -c(1, 2)]
+  tbl2 <- cbind(tbl2, Total = rowSums(tbl2[-1], na.rm = TRUE))
+  colznmz <- colnames(tbl2)[-1]
+  colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
+  tbl1 %>% left_join(tbl2)
 }
+
+altstest <- AltsTable(USQ)
+
 ### STILL NEED TO UPDATE
 #-----Month table function
 MonthTable <- function(df1){
