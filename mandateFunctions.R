b34609a16b0b542e2791e6ceb07c6abfdebf94ed
Katie <katie.segreti@gmail.com>
2018-04-07 14:54:26 -0400

    update mandate functions

--- mandateFunctions.R
+++ mandateFunctions.R
@@ -1,8 +1,6 @@
 #--------------------------------------------
 ###### GLOBAL FUNCTIONS ################
-#is even and is odd functions
-is.even <- function(x){x %%2 == 0}
-is.odd <- function(x){x %%2 == 1}
+
 ### paste number at end of column names so you can join
 pasteNumber <- function(x, y) {  paste(x, y)}
 ##total number of mandates: df, 2 variables
@@ -40,17 +38,14 @@ PNCtable <- function(df) {
   combined <- combined[,c(1,4,8,3,7,2,6,5,9)]
   return(combined)
 }
-USPNC <- PNCtable(USQ)
 
 ####### FTPtable the new way 
-## still have to get totals column
-FTtableNEW <- function(df) {
+FTPtable <- function(df) {
   tbl1 <- totalN(df, FundType, MainAssetClass)
   tbl2 <- totalN(df, FundType, MandateRegion)
   combined <- tbl1 %>% left_join(tbl2)
   return(combined)
 }
-USP_FT <- FTtableNEW(USQP)
 
 ### updated
 #---- Asset class by investment region
@@ -62,9 +57,6 @@ ACbyMRtable <- function(df) {
   tbl1 %>% left_join(tbl2)
 }
 
-USQAC1 <- ACbyMRtable(USQP)
-
-
 ### UPDATED
 #---- SubAsset class by investment region for assete class
 SACbyMRtable <- function(df1, assetclass) {
@@ -75,8 +67,6 @@ SACbyMRtable <- function(df1, assetclass) {
   colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
   tbl1 %>% left_join(tbl2)
 }
-
-SACtest1 <- SACbyMRtable(mandates, "Commodities")
 
 
 ### UPDATED
@@ -90,9 +80,7 @@ IAbyMRtable <- function(df1, assetclass) {
   colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
   tbl1 %>% left_join(tbl2)
 }
-IAtest <- IAbyMRtable(US12mo, "Equity")
 
-  
 ### UPDATED
 #---- style by investment region for equity 
 
@@ -104,7 +92,6 @@ EQStyletable <- function(df1) {
   colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
   tbl1 %>% left_join(tbl2)
 }
-eqstest <- EQStyletable(mandates)
 
 ### UPDATED
 EQCaptable <- function(df1) {
@@ -115,8 +102,6 @@ EQCaptable <- function(df1) {
   colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
   tbl1 %>% left_join(tbl2)
 }
-eqctest <- EQCaptable(mandates)
-
 
 ### UPDATED
 # alts table
@@ -147,91 +132,51 @@ AltsTable <- function(df1) {
   tbl1 %>% left_join(tbl2)
 }
 
-altstest <- AltsTable(USQ)
-
-### STILL NEED TO UPDATE
+### UPDATED
 #-----Month table function
-MonthTable <- function(df1){
-  monthz <- c("01","02","03","04","05","06","07","08", "09","10","11","12")
-  monthz2 <- c("Jan","Feb","March","April","May","June","July","Aug","Sept", "Oct","Nov","Dec")
-  totalz <- c("Total#", "Total$")
-  z <- rep(0, length(monthz)*length(totalz))
-  m1 <- matrix(z, nrow=length(monthz), ncol=length(totalz))
-  rownames(m1) <- monthz2
-  colnames(m1) <- totalz
-  for(i in 1:length(monthz)){
-    m1[i,1] <- nrow(df1[df1$Month==monthz[i],])
-    m1[i,2] <- sum(df1[df1$Month==monthz[i], "MandateSizeAmount"])
-  }
-  return(m1)
+MonthTable <- function(df1) {
+  moz <- df1 %>%
+    group_by(Month) %>%
+    summarise(Total = n(), SumTotal = sum(MandateSizeAmount)) %>%
+    mutate(Month = month.abb[as.integer(Month)]) %>%
+    replace(., is.na(.), 0)
+  moz2 <- rbind(moz, c("Total", colSums(moz[-1])))
+  return(moz2)
 }
-### STILL NEED TO UPDATE
+
+### UPDATED
 #-----Year Table Function
-YearTable <- function(df1){
-  yrs <- c("2015","2016","2017")
-  AC <- levels(droplevels(df1$MainAssetClass))
-  yearz <- rep(yrs, times=1, each=2)
-  z <- rep(0, length(yearz)*length(AC))
-  #create matrix
-  m1 <- matrix(z, nrow=length(AC), ncol=length(yearz))
-  rownames(m1) <- AC
-  colnames(m1) <- yearz
-  #fill matrix
-  for(i in 1:length(AC)){
-    for(j in 1:length(yearz)){
-      if(is.odd(j)){
-        m1[i,j] <- nrow(df1[df1$MainAssetClass==AC[i]&df1$Year==yearz[j],])
-      }else{
-        m1[i,j] <- sum(df1[df1$MainAssetClass==AC[i]&df1$Year==yearz[j], "MandateSizeAmount"])
-      }
-    }
-  }
-  return(m1)
+YearTable <- function(df1) {
+  tbl1 <- totalN(df1, MainAssetClass, Year)
+  tbl2 <- totalSum(df1, MainAssetClass, Year)
+  colznmz <- colnames(tbl2)[-1]
+  colnames(tbl2)[-1] <- map_chr(colznmz, pasteNumber, 1)
+  tbl1 %>% left_join(tbl2)
 }
-### STILL NEED TO UPDATE
+
+###UPDATED
 #----FUNCTION: top consultants: # and $
-consTable <- function(df1, sortz=1){
-  consz <- levels(droplevels(df1$SearchConsultant))
-  colz <- c("Total1", "Total2")
-  z <- rep(0, length(consz)*length(colz))
-  #create matrix
-  m1 <- matrix(z, nrow=length(consz), ncol=length(colz))
-  rownames(m1) <- consz
-  colnames(m1) <- colz
-  #fill matrix
-  for(i in 1:length(consz)){
-    m1[i,1] <- nrow(df1[df1$SearchConsultant==consz[i],])
-    m1[i,2] <- sum(df1[df1$SearchConsultant==consz[i],"MandateSizeAmount"])
+consTable <- function(df1, sortz = 1) {
+  conz <- df1 %>%
+    group_by(SearchConsultant) %>%
+    summarise(Total = n(), SumTotal = sum(MandateSizeAmount)) %>%
+    replace(., is.na(.), 0)
+  if(sortz != 1){
+    conz <- arrange(conz, desc(SumTotal), desc(Total))
+  } else {
+    conz <- arrange(conz, desc(Total), desc(SumTotal))
   }
-  if(sortz==1){
-    m2 <- m1[order(m1[,1],m1[,2],decreasing = TRUE),]
-  }else{
-    m2 <- m1[order(m1[,2],m1[,1],decreasing = TRUE),]
-  }
-  
-  return(m2)
+  return(conz)
 }
-### STILL NEED TO UPDATE
+
+###UPDATED
 #---function - search consultant matrix
-consMX <- function(df0){
-  df1 <- df0[df0$SearchConsultant!="Unknown",]
-  SC <- levels(droplevels(df1$SearchConsultant))
-  FT <- levels(droplevels(df1$FundType))
-  AC <- levels(droplevels(df1$MainAssetClass))
-  MR <- levels(droplevels(df1$MandateRegion))
-  colz <- c("Total", FT, AC, MR)
-  z <- rep(0, length(SC)*length(colz))
-  #create matrix
-  m1 <- matrix(z, nrow=length(SC), ncol=length(colz))
-  rownames(m1) <- SC
-  colnames(m1) <- colz
-  #fillmatrix
-  for(i in 1:length(SC)){
-    m1[i,1] <- nrow(df1[df1$SearchConsultant==SC[i],])
-    for(j in 2:length(colz)){
-      m1[i,j] <- nrow(df1[df1$SearchConsultant==SC[i]&(df1$FundType==colz[j]|df1$MainAssetClass==colz[j]|
-                                                         df1$MandateRegion==colz[j]),])
-    }
-  }
-  return(m1)
+consMX <- function(df0) {
+  df1 <- filter(df0, SearchConsultant != "Unknown")
+  tbl1 <- totalN(df1, SearchConsultant, FundType)
+  tbl2 <- totalN(df1, SearchConsultant, MainAssetClass)
+  tbl3 <- totalN(df1, SearchConsultant, MandateRegion)
+  combined <- tbl1 %>% left_join(tbl2)
+  combined <- combined %>% left_join(tbl3)
+  return(combined)
 }
